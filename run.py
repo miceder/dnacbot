7983fb58196e87b4e2d78049ff9df7d8b5477363
miceder <miceder@cisco.com>
2019-07-16 09:18:52 +0200

    ssl

--- run.py
+++ run.py
@@ -74,4 +74,4 @@ def dnacPayload():
         return("Connection Alive")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000,threaded=True,debug=False)
+    app.run(host="0.0.0.0",port=5000,threaded=True,debug=False,ssl_context='adhoc')
